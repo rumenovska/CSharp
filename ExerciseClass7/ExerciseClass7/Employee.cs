efb0479b7b62cc29ec21efd7627c370b80abbd9c
Marija Rumenovska <marija.rumenovska@hotmail.com>
2019-03-14 20:49:01 +0100

    class 7

---
+++ ExerciseClass7/ExerciseClass7/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseClass7
+{
+	class Employee
+	{
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Role Role { get; set; }
+		protected double Salary { get; set; }
+
+		public Employee() { 
+}
+		public void PrintInfo()
+		{
+			Console.WriteLine($"Name: {this.FirstName} {this.LastName}, Salary= {Salary}");
+		}
+
+		public virtual double GetSalary()
+		{
+			return this.Salary;
+
+
+		}
+	}
+}
