6dd3b1f9c5dc429285844589e5abb4a03ac3b117
Marija Rumenovska <marija.rumenovska@hotmail.com>
2019-04-09 20:21:40 +0200

    class6

---
+++ FuncAction/FuncAction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> IsPalindrome = text => {
+                string reverse = String.Join("", text.Reverse());
+                if(text.ToLower() == reverse.ToLower())
+                {
+                    Console.WriteLine("Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine("Not Palindrome");
+                }
+            };
+            Action<string> WordCount = text => { Console.WriteLine($"{text.Split().Length}"); };
+            Action<string> ReverseText = text => {
+                string reverse = String.Join("", text.Reverse());
+                Console.WriteLine($"{reverse}");
+            };
+
+            IsPalindrome += WordCount;
+            IsPalindrome += ReverseText;
+            IsPalindrome("Anna");
+            IsPalindrome("How are you?");
+            Console.ReadLine();
+
+        }
+    }
+}
