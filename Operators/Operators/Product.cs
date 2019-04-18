5a239bce567b376098a53723b4cb7fdda023c616
Marija Rumenovska <marija.rumenovska@hotmail.com>
2019-04-18 19:24:54 +0200

    Operators

---
+++ Operators/Operators/Product.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Operators
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+        public Product(string name, int qunatity, double price)
+        {
+            this.Name = name;
+            this.Quantity = qunatity;
+            this.Price = price;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name} || Quantity: {Quantity} || Price: {Price}");
+        }
+        public static Product operator + (Product p1, Product p2)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name} + {p2.Name}";
+            if(p1.Quantity > p2.Quantity)
+            {
+                product.Quantity = p1.Quantity;
+            }
+            else
+            {
+                product.Quantity = p2.Quantity;
+            }
+            product.Price = p1.Price + p2.Price;
+            return product;
+        }
+        public static Product operator -(Product p1, Product p2)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name} - {p2.Name}";
+            if (p1.Quantity > p2.Quantity)
+            {
+                product.Quantity = p1.Quantity;
+            }
+            else
+            {
+                product.Quantity = p2.Quantity;
+            }
+            product.Price = p1.Price - p2.Price;
+            return product;
+        }
+
+        public static Product operator /(Product p1, Product p2)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name} / {p2.Name}";
+            if (p1.Quantity > p2.Quantity)
+            {
+                product.Quantity = p1.Quantity;
+            }
+            else
+            {
+                product.Quantity = p2.Quantity;
+            }
+            product.Price = p1.Price / p2.Price;
+            return product;
+        }
+        public static Product operator *(Product p1, Product p2)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name} * {p2.Name}";
+            if (p1.Quantity > p2.Quantity)
+            {
+                product.Quantity = p1.Quantity;
+            }
+            else
+            {
+                product.Quantity = p2.Quantity;
+            }
+            product.Price = p1.Price * p2.Price;
+            return product;
+        }
+        public static Product operator %(Product p1, Product p2)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name} % {p2.Name}";
+            if (p1.Quantity > p2.Quantity)
+            {
+                product.Quantity = p1.Quantity;
+            }
+            else
+            {
+                product.Quantity = p2.Quantity;
+            }
+            product.Price = p1.Price % p2.Price;
+            return product;
+        }
+        public static Product operator ++(Product p1)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name}++";
+            product.Quantity = p1.Quantity ++;
+            
+            product.Price = p1.Price ++;
+            return product;
+        }
+
+        public static Product operator --(Product p1)
+        {
+            Product product = new Product();
+            product.Name = $"{p1.Name}--";
+            product.Quantity = p1.Quantity--;
+
+            product.Price = p1.Price--;
+            return product;
+        }
+        public static bool operator !(Product p1)
+        {
+            return p1.Quantity == 0;
+        }
+
+        public static Product operator +( Product p1, int a)
+        {
+            return new Product($"{p1.Name} + {a}", p1.Quantity+a, p1.Price);
+        }
+
+    }
+}
---
+++ Operators/OperatorsDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Operators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OperatorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Milk", 5, 50);
+            Product p2 = new Product("Bread", 2, 28);
+            var p3 = p1 + p2;
+            p3.Print();
+
+            var p4 = p1 - p2;
+            p4.Print();
+
+            var p5 = p1 / p2;
+            p5.Print();
+
+            var p6 = p1 * p2;
+            p6.Print();
+            var p7 = p1 % p2;
+            p7.Print();
+
+            Console.WriteLine(!p1);
+            var p8 = p1 + 7;
+            p8.Print();
+
+            p2++.Print();
+            p1--.Print();
+            Console.ReadLine();
+        }
+    }
+}
